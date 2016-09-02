0becb591df8c59625ac05bd9103620c7dfbb03b2
Bryan <b.smith33@hotmail.com>
2016-09-02 11:23:23 -0600

    removing ui router for now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { UIROUTER_DIRECTIVES } from 'ui-router-ng2';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
-  directives: [UIROUTER_DIRECTIVES]
+  directives: [LoginComponent]
 })
 export class AppComponent {
   
